22bdca7e4cd341ea76ff30a36425cb7ae27f2143
TD_SoftwareEngr <tobidelly@gmail.com>
2024-12-16 22:35:22 +0100

    Returning Navbar in Function App via React router, next is to declare all route paths so when Navlinks are clicked, it takes you to the necessary page

--- src/App.tsx
+++ src/App.tsx
@@ -2,3 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
+function App() {
+    return (
+        <Router>
+            <div className="flex flex-col min-h-screen">
+                <Navbar />
+                <main className="flex-grow">
+          <Routes></Routes>
+}
