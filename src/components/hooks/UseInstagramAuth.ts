49eef28e51e9ec8fe3d3e34d63a91448b2e4531c
TD_SoftwareEngr <tobidelly@gmail.com>
2024-12-16 23:47:35 +0100

    export function useInstagramAuth and declaring and initializing authenticate as constant

--- src/components/hooks/UseInstagramAuth.ts
+++ src/components/hooks/UseInstagramAuth.ts
@@ -1,2 +1,9 @@
 import { useState, useCallback } from 'react';
 import { User } from '../types';
+
+export function useInstagramAuth() {
+  const [authenticating, setAuthenticating] = useState(false);
+
+  const authenticate = useCallback(async (username: string): Promise<User> => {
+    setAuthenticating(true);
+    
