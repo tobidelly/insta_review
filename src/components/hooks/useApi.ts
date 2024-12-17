b4f84018bfaa76234c1bf0c81bc3d61e12f51069
TD_SoftwareEngr <tobidelly@gmail.com>
2024-12-17 08:22:43 +0100

    React hooks, useVendors and useVendor, to simplify data fetching for vendors using react-query library and Axios for HTTP requests

--- src/components/hooks/useApi.ts
+++ src/components/hooks/useApi.ts
@@ -3,3 +3,19 @@ import { AxiosError } from 'axios';
 import { vendorAPI, reviewAPI, authAPI } from '../services/api';
 import type { Vendor, Review, User } from '../types';
 
+// Vendors
+export const useVendors = (options?: UseQueryOptions<Vendor[], AxiosError>) => {
+  return useQuery<Vendor[], AxiosError>(
+    ['vendors'],
+    () => vendorAPI.getAll(),
+    options
+  );
+};
+
+export const useVendor = (username: string, options?: UseQueryOptions<Vendor, AxiosError>) => {
+  return useQuery<Vendor, AxiosError>(
+    ['vendor', username],
+    () => vendorAPI.getByUsername(username),
+    options
+  );
+};
