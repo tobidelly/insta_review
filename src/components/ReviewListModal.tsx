1bab8d5c58969ffc2f67d81755527e72e6680e52
TD_SoftwareEngr <tobidelly@gmail.com>
2024-12-28 05:23:53 +0100

    importing Vendor & Review from type declaration in types.tsx and also useVendorReviews hook

---
+++ src/components/ReviewListModal.tsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import { X, Star, ChevronDown, BadgeCheck } from 'lucide-react';
+import { Vendor, Review } from '../types';
+import { useVendorReviews } from '../hooks/useApi';
+
