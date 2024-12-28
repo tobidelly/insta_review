2771a0478b4a0a80ceb5601ec6d7040cec55c72c
TD_SoftwareEngr <tobidelly@gmail.com>
2024-12-28 05:18:33 +0100

    I'll need to import VendorCard, SearchBar, Pagination and mockData from data for the homepage. Now I can go create these components, and finish HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,2 +1,6 @@
 import React, { useState } from 'react';
 import { Search, Star, Shield, AlertTriangle, ThumbsUp } from 'lucide-react';
+import VendorCard from '../components/VendorCard';
+import SearchBar from '../components/SearchBar';
+import Pagination from '../components/Pagination';
+import { mockVendors } from '../data/mockData';
