6b91cb4fbedf95f075b58405a335fa867838f99e
TD_SoftwareEngr <tobidelly@gmail.com>
2024-12-28 14:20:55 +0100

    import mockVendors from the mockData and AddMissingVendorModal so users can add missing vendors too

---
+++ src/pages/ReportScam.tsx
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import { AlertTriangle, Search, Shield } from 'lucide-react';
+import AddMissingVendorModal from '../components/AddMissingVendorModal';
+import { mockVendors } from '../data/mockData';
