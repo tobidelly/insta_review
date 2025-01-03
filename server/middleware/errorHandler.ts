e38e6a1bd3add2d2f07396bb7ed8b7af8c5abb1b
TD_SoftwareEngr <tobidelly@gmail.com>
2025-01-03 08:22:26 +0100

    updating errorHandler.ts codes from JavaScript to TypeScript

--- server/middleware/errorHandler.ts
+++ server/middleware/errorHandler.ts
@@ -1,6 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
 import { ZodError } from 'zod';
 
-export const errorHandler = (err, req, res, next) => {
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   console.error(err);
 
   if (err instanceof ZodError) {
